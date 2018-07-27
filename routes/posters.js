5a84c156509d627400926b41a4a5aacf18372303
Pavan Boro <pavanboro14@gmail.com>
2018-07-27 11:51:19 +0530

    testing heroku app error

---
+++ routes/posters.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+
+//MULTER CONFIG: to get file photos to temp server storage
+const multerConfig = {
+    
+  storage: multer.diskStorage({
+   //Setup where the user's file will go
+   destination: function(req, file, next){
+     next(null, 'uploads/posters/');
+     },   
+      
+      //Then give the file a unique name
+      filename: function(req, file, next){
+          console.log(file);
+          const ext = file.mimetype.split('/')[1];
+          next(null, file.fieldname + '-' + Date.now() + '.'+ext);
+        }
+      }),   
+      
+      //A means of ensuring only images are uploaded. 
+      fileFilter: function(req, file, next){
+            if(!file){
+              next();
+            }
+          const image = file.mimetype.startsWith('image/');
+          if(image){
+            console.log('photo uploaded');
+            next(null, true);
+          }else{
+            console.log("file not supported");
+            
+            //TODO:  A better message response to user on failure.
+            return next();
+          }
+      }
+    };
+
+router.post('/',multer(multerConfig).single('photo'),function(req,res){
+    res.send('Complete!');
+});
+
+
+module.exports = router;
